fcfc622d4d3160d326a91e6bc90c0ecb0e69e994
Vladosjan <150368423+Vladosjan@users.noreply.github.com>
2024-09-07 13:37:15 +0300

    Create Bohza.jsx

---
+++ Bohza.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+function BohzaAlexei() 
+{ 
+return( 
+<div>
+    <h1>This is a Bohza Alexei!</h1> 
+    <h2>Big  Alexei</h2>
+    </div>
+); 
+} 
+export default BohzaAlexei;
